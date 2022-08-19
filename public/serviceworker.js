b01b978bd72a6915e99b026f8e1973656cc6a3b2
indrajith <febin@eguidancelabs.com>
2022-08-19 13:35:48 +0530

    edited service worker and manifest

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,8 +1,15 @@
 //cache= storage of browser
 const CACHE_NAME = "version-1";
 const urlsToCache = [
-    "/",
-    "index.html"
+  "/",
+  "index.html",
+  "manifest.json",
+  "assets/logo.png",
+  "assets/maskable.png",
+  "favicon.ico",
+  "assets/apple-touch-icon.png",
+  "assets/logo-192.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css",
 ];
 
 const self = this;
@@ -19,13 +26,40 @@ self.addEventListener("install", (event) => {
 });
 
 // Listen for requests , deals with cached file while offline
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       console.log("fetch cache", event.request);
+
+//       return fetch(event.request).catch(() => caches.match("index.html"));
+//     })
+//   );
+// });
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(() => {
-        console.log("fetch cache");
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          console.log("found", event.request.url, "in cache");
+          return response;
+        }
 
-      return fetch(event.request).catch(() => caches.match("index.html"));
-    })
+        console.log("not in canche", event.request.url);
+
+        return fetch(event.request).then((response) => {
+          if (response.status === 404) {
+            return caches.match("/index.html");
+          }
+
+          return response;
+        });
+      })
+      .catch((error) => {
+        console.error("failed to fetch", event.request.url);
+        return caches.match("/index.html");
+      })
   );
 });
 
@@ -47,14 +81,17 @@ self.addEventListener("activate", (event) => {
   cacheWhitelist.push(CACHE_NAME);
 
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
         cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
+          // if (!cacheWhitelist.includes(cacheName)) {
+          //   return caches.delete(cacheName);
+          //}
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
-      )
-    )
+      );
+    })
   );
 });
